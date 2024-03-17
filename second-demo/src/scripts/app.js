70eae97eb80cd10b05157224ce8cc8160f356219
NamDP <ealflm@gmail.com>
2024-03-17 20:01:21 +0700

    commit

--- second-demo/src/scripts/app.js
+++ second-demo/src/scripts/app.js
@@ -1,8 +1,8 @@
-import 'styles/index.scss';
-import Cone from './elements/cone';
-import Box from './elements/box';
-import Torus from './elements/torus';
-import { radians, map, distance, hexToRgbTreeJs } from './helpers';
+import "styles/index.scss";
+import Cone from "./elements/cone";
+import Box from "./elements/box";
+import Torus from "./elements/torus";
+import { radians, map, distance, hexToRgbTreeJs } from "./helpers";
 
 export default class App {
   setup() {
@@ -10,7 +10,7 @@ export default class App {
 
     this.raycaster = new THREE.Raycaster();
 
-    this.backgroundColor = '#1b1b1b';
+    this.backgroundColor = "#1b1b1b";
     this.gutter = { size: 1.2 };
     this.meshes = [];
     this.grid = { cols: 15, rows: 7 };
@@ -18,21 +18,15 @@ export default class App {
     this.height = window.innerHeight;
     this.mouse3D = new THREE.Vector2();
     this.repulsion = 1;
-    this.geometries = [
-      new Box(),
-      new Torus(),
-      new Cone()
-    ];
+    this.geometries = [new Box(), new Torus(), new Cone()];
 
-    const gui = this.gui.addFolder('Background');
-
-    gui.addColor(this, 'backgroundColor').onChange((color) => {
-      document.body.style.backgroundColor = color;
+    window.addEventListener("resize", this.onResize.bind(this), {
+      passive: true,
     });
 
-    window.addEventListener('resize', this.onResize.bind(this), { passive: true });
-
-    window.addEventListener('mousemove', this.onMouseMove.bind(this), { passive: true });
+    window.addEventListener("mousemove", this.onMouseMove.bind(this), {
+      passive: true,
+    });
 
     this.onMouseMove({ clientX: 0, clientY: 0 });
   }
@@ -60,48 +54,30 @@ export default class App {
   }
 
   addAmbientLight() {
-    const obj = { color: '#2900af' };
+    const obj = { color: "#2900af" };
     const light = new THREE.AmbientLight(obj.color, 1);
 
     this.scene.add(light);
-
-    const gui = this.gui.addFolder('Ambient Light');
-
-    gui.addColor(obj, 'color').onChange((color) => {
-      light.color = hexToRgbTreeJs(color);
-    });
   }
 
   addSpotLight() {
-    const obj = { color: '#e000ff' };
+    const obj = { color: "#e000ff" };
     const light = new THREE.SpotLight(obj.color, 1, 1000);
 
     light.position.set(0, 27, 0);
     light.castShadow = true;
 
     this.scene.add(light);
-
-    const gui = this.gui.addFolder('Spot Light');
-
-    gui.addColor(obj, 'color').onChange((color) => {
-      light.color = hexToRgbTreeJs(color);
-    });
   }
 
   addRectLight() {
-    const obj = { color: '#0077ff' };
+    const obj = { color: "#0077ff" };
     const rectLight = new THREE.RectAreaLight(obj.color, 1, 2000, 2000);
 
     rectLight.position.set(5, 50, 50);
     rectLight.lookAt(0, 0, 0);
 
     this.scene.add(rectLight);
-
-    const gui = this.gui.addFolder('Rect Light');
-
-    gui.addColor(obj, 'color').onChange((color) => {
-      rectLight.color = hexToRgbTreeJs(color);
-    });
   }
 
   addPointLight(color, position) {
@@ -112,31 +88,22 @@ export default class App {
   }
 
   getRandomGeometry() {
-    return this.geometries[Math.floor(Math.random() * Math.floor(this.geometries.length))];
+    return this.geometries[
+      Math.floor(Math.random() * Math.floor(this.geometries.length))
+    ];
   }
 
   createGrid() {
     this.groupMesh = new THREE.Object3D();
 
     const meshParams = {
-      color: '#ff00ff',
-      metalness: .58,
-      emissive: '#000000',
-      roughness: .18,
+      color: "#ff00ff",
+      metalness: 0.58,
+      emissive: "#000000",
+      roughness: 0.18,
     };
 
     const material = new THREE.MeshPhysicalMaterial(meshParams);
-    const gui = this.gui.addFolder('Mesh Material');
-
-    gui.addColor(meshParams, 'color').onChange((color) => {
-      material.color = hexToRgbTreeJs(color);
-    });
-    gui.add(meshParams, 'metalness', 0.1, 1).onChange((val) => {
-      material.metalness = val;
-    });
-    gui.add(meshParams, 'roughness', 0.1, 1).onChange((val) => {
-      material.roughness = val;
-    });
 
     for (let row = 0; row < this.grid.rows; row++) {
       this.meshes[row] = [];
@@ -145,7 +112,11 @@ export default class App {
         const geometry = this.getRandomGeometry();
         const mesh = this.getMesh(geometry.geom, material);
 
-        mesh.position.set(col + (col * this.gutter.size), 0, row + (row * this.gutter.size));
+        mesh.position.set(
+          col + col * this.gutter.size,
+          0,
+          row + row * this.gutter.size,
+        );
         mesh.rotation.x = geometry.rotationX;
         mesh.rotation.y = geometry.rotationY;
         mesh.rotation.z = geometry.rotationZ;
@@ -161,8 +132,10 @@ export default class App {
       }
     }
 
-    const centerX = ((this.grid.cols - 1) + ((this.grid.cols - 1) * this.gutter.size)) * .5;
-    const centerZ = ((this.grid.rows - 1) + ((this.grid.rows - 1) * this.gutter.size)) * .5;
+    const centerX =
+      (this.grid.cols - 1 + (this.grid.cols - 1) * this.gutter.size) * 0.5;
+    const centerZ =
+      (this.grid.rows - 1 + (this.grid.rows - 1) * this.gutter.size) * 0.5;
 
     this.groupMesh.position.set(-centerX, 0, -centerZ);
 
@@ -179,16 +152,19 @@ export default class App {
   }
 
   addCameraControls() {
-    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+    this.controls = new THREE.OrbitControls(
+      this.camera,
+      this.renderer.domElement,
+    );
   }
 
   addFloor() {
     const geometry = new THREE.PlaneGeometry(2000, 2000);
-    const material = new THREE.ShadowMaterial({ opacity: .3 });
+    const material = new THREE.ShadowMaterial({ opacity: 0.3 });
 
     this.floor = new THREE.Mesh(geometry, material);
     this.floor.position.y = 0;
-    this.floor.rotateX(- Math.PI / 2);
+    this.floor.rotateX(-Math.PI / 2);
     this.floor.receiveShadow = true;
 
     this.scene.add(this.floor);
@@ -246,30 +222,38 @@ export default class App {
 
       for (let row = 0; row < this.grid.rows; row++) {
         for (let col = 0; col < this.grid.cols; col++) {
-
           const mesh = this.meshes[row][col];
 
-          const mouseDistance = distance(x, z,
+          const mouseDistance = distance(
+            x,
+            z,
             mesh.position.x + this.groupMesh.position.x,
-            mesh.position.z + this.groupMesh.position.z);
+            mesh.position.z + this.groupMesh.position.z,
+          );
 
           const y = map(mouseDistance, 6, 0, 0, 10);
-          TweenMax.to(mesh.position, .2, { y: y < 1 ? 1 : y });
+          TweenMax.to(mesh.position, 0.2, { y: y < 1 ? 1 : y });
 
           const scaleFactor = mesh.position.y / 2.5;
           const scale = scaleFactor < 1 ? 1 : scaleFactor;
 
-          TweenMax.to(mesh.scale, .4, {
+          TweenMax.to(mesh.scale, 0.4, {
             ease: Expo.easeOut,
             x: scale,
             y: scale,
             z: scale,
           });
 
-          TweenMax.to(mesh.rotation, .5, {
+          TweenMax.to(mesh.rotation, 0.5, {
             ease: Expo.easeOut,
             x: map(mesh.position.y, -1, 1, radians(45), mesh.initialRotation.x),
-            z: map(mesh.position.y, -1, 1, radians(-90), mesh.initialRotation.z),
+            z: map(
+              mesh.position.y,
+              -1,
+              1,
+              radians(-90),
+              mesh.initialRotation.z,
+            ),
             y: map(mesh.position.y, -1, 1, radians(90), mesh.initialRotation.y),
           });
         }
